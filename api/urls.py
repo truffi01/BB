f528d35bd747348437d99aa58a7d32fadf28cdd0
Thomas Ruffino <thomasruffino1@gmail.com>
2019-12-14 09:51:41 -0500

    finmished API for backend and checked with postman

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,10 @@ from rest_framework import routers
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include 
+from .views import ProductViewSet
 
 router = routers.DefaultRouter()
+router.register('product', ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
