92c8a33414af8342983fb3115b6fc9c0b832a66b
Thomas Ruffino <thomasruffino1@gmail.com>
2019-12-18 15:31:38 -0500

    added login feature

--- bbapp/src/components/loginIn.js
+++ bbapp/src/components/loginIn.js
@@ -1,8 +1,47 @@
 import React, { Component } from 'react'; 
 
 class Login extends Component {
+
+    state = {
+        credentials: {
+            username: "",
+            password:"",
+        }
+    }
+
+    inputChanged = event => {
+        let cred = this.state.credentials;
+        cred[event.target.name] = event.target.value;
+        this.setState({
+          credentials: cred 
+        });
+      }
+
+      login = event => {
+          console.log(this.state.credentials)
+          fetch(`${process.env.REACT_APP_API_URL}/auth/`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json', 
+            },
+            body: JSON.stringify(this.state.credentials)
+          }).then(res => res.json())
+          .then(resp => {
+              console.log(resp.token);
+              window.location.href ="/product";
+          })
+          .catch(err => console.log(err)) 
+      }
+
     render(){
-        return <h1>Login</h1>
+        return <div>
+            <h2>Login</h2>
+            <span>Username</span><br/>
+            <input type="text" name ="username" value={this.state.credentials.username} onChange={this.inputChanged} /><br/>
+            <span>Password</span><br/>
+            <input type="password" name ="password" value={this.state.credentials.password} onChange={this.inputChanged} /><br/>
+            <button onClick={this.login}>Login</button>
+        </div>
     }
 }
 
