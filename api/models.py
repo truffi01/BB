7c976d0ae4ee97619147e01e8ed9d60636c4b569
Thomas Ruffino <thomasruffino1@gmail.com>
2020-01-02 14:08:33 -0500

    fimnished debugging user connection to posts in the serializers.

--- api/models.py
+++ api/models.py
@@ -7,14 +7,7 @@ from crum import get_current_user
 class Product(models.Model):
     name = models.CharField(max_length=85)
     description = models.TextField(max_length=360)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True, default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, default=None)
 
-    def save(self, *args, **kwargs):
-        user = get_current_user()
-        if user and not user.pk:
-            user = None
-        if not self.pk:
-            self.created_by = user
-        self.modified_by = user
-        super(Product, self).save(*args, **kwargs)
+    
  
--- api/serializers.py
+++ api/serializers.py
@@ -2,12 +2,17 @@ from rest_framework import serializers
 from .models import Product 
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User 
-from crum import get_current_user;
+from crum import get_current_user
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model= Product
-        fields = ('id', 'name', 'description',) 
+        fields = ('id', 'name', 'description',)
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data.update(user=user)
+        return super().create(validated_data)
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
